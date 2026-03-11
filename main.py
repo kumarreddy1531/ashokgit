f4639427acf0553be1bbf962ac21d173c1c9c8d3
Lakkireddyashok Reddy <lakkireddyashok.reddy@fractal.ai>
2026-03-11 19:13:19 +0530

    Modified code for Azure App Service

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {"status": "ok", "message": "FastAPI CI/CD on Azure App Service (F1) is working!"}
+    return {"status": "ok", "message": "FastAPI CI/CD on Azure App Service (F1) is working fine!"}
